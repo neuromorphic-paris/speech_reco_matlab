6dbe2f99d21f38188f485b37812ae01b6742664a
Vincent Maison <pro@vincent.maison>
2018-09-13 14:48:17 +0200

    Added max number of files

--- get_filenames_on_off_database.m
+++ get_filenames_on_off_database.m
@@ -1,4 +1,4 @@
-function [filenames_train, class_train, filenames_test, class_test] = get_filenames_on_off_database()
+function [filenames_train, class_train, filenames_test, class_test] = get_filenames_on_off_database(nb_files)
 
 %% Training and testing files as suggested
 training_files = table2array(readtable(...
@@ -25,6 +25,16 @@ for ind = 1:numel(used_classes)
   files_with_curr_class = cellfun(@(x) strcmp(x, curr_class), training_files(:,1));
 
   curr_files_training = training_files(files_with_curr_class, :);
+  nb_files_curr_class = numel(find(files_with_curr_class));
+  if exist('nb_files', 'var')
+    if nb_files>nb_files_curr_class
+      error('Number of files argument is too big')
+    else
+      perm = randperm(nb_files_curr_class, nb_files);
+      curr_files_training = curr_files_training(perm,:);
+    end
+  end
+
   for ind_files = 1:size(curr_files_training,1)
     curr_filename = fullfile(folders{ind}, ...
       [curr_files_training{ind_files,2}, '.aedat']);
@@ -39,6 +49,16 @@ for ind = 1:numel(used_classes)
   end
 
   curr_files_testing = testing_files(files_with_curr_class, :);
+  nb_files_curr_class = numel(find(files_with_curr_class));
+  if exist('nb_files', 'var')
+    if nb_files>nb_files_curr_class
+      error('Number of files argument is too big')
+    else
+      perm = randperm(nb_files_curr_class, nb_files);
+      curr_files_testing = curr_files_testing(perm,:);
+    end
+  end
+
   for ind_files = 1:size(curr_files_testing,1)
     curr_filename = fullfile(folders{ind}, ...
       [curr_files_testing{ind_files,2}, '.aedat']);
